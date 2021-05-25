f7f38850ec690bc1a3e1b555514346758acb954f
Pornchai Soda <Pornchai_Soda@Jabil.com>
2021-05-25 17:04:47 +0800

    separate config file for multiples pc

--- plotmanager/library/parse/configuration.py
+++ plotmanager/library/parse/configuration.py
@@ -1,6 +1,7 @@
 import pathlib
 import os
 import yaml
+import platform
 
 
 from plotmanager.library.utilities.exceptions import InvalidYAMLConfigException
@@ -8,7 +9,10 @@ from plotmanager.library.utilities.exceptions import InvalidYAMLConfigException
 
 def _get_config():
     directory = pathlib.Path().resolve()
-    file_name = 'config.yaml'
+    try:
+        file_name = 'config-{}.yaml'.format(platform.node())
+    except Exception:
+        file_name = 'config.yaml'
     file_path = os.path.join(directory, file_name)
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"Unable to find the config.yaml file. Expected location: {file_path}")
